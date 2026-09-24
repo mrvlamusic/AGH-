7a9e76577acce0d5ff539edcff079e0c9e58f828
mrvlamusic <mrvlamusic@gmail.com>
2026-09-24 00:30:09 -0400

    feat: implement apps/api/src/main.ts

---
+++ apps/api/src/main.ts
@@ -0,0 +1,11 @@
+import "reflect-metadata";
+import { NestFactory } from "@nestjs/core";
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  app.enableCors({ origin: true, credentials: true });
+  await app.listen({ port: 4000, host: "0.0.0.0" });
+}
+bootstrap();
